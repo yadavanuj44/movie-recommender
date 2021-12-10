b8a69b4e596df4ca27159febaedebb548992c0a9
yadavanuj44 <95302917+yadavanuj44@users.noreply.github.com>
2021-12-10 08:48:57 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const songsDictionary = {
+const moviesDictionary = {
   Action: [
-    { Song: "Tujhe Kitna Chahne Lage Hum", Movie: "kabir Singh" },
-    { Song: "Jaan Nissar", Movie: "Kedarnath" },
-    { Song: "Ek tu hi yar mera", Movie: "Pati patni aur woh" }
+    { Movie: "Black Panther", Year: "2018" },
+    { Movie: "Mad Max: Fury Road", Year: "2015" },
+    { Movie: "Thor: Ragnarok", Year: "2017" }
   ],
   Horror: [
-    { Song: "Yeh Ishq Hai", Movie: "Jab We Met" },
-    { Song: "Hasi", Movie: "Hamari Adhuri Kahani" },
-    { Song: "Teri meri", Movie: "Bodyguard" }
+    { Movie: "Get Out", Year: "2017" },
+    { Movie: "A Quiet Place", Year: "2018" },
+    { Movie: "The Evil Dead", Year: "1981" }
   ],
   Comedy: [
-    { Song: "Yeh raat bheegi bheegi", Movie: "Chori Chori" },
-    { Song: "Hai apna dil to awara", Movie: "Solva Saal" },
-    { Song: "Lag jaa gale", Movie: "Woh jo hasina" }
+    { Movie: "Once Upon a Time In Hollywood", Year: "2019" },
+    { Movie: "Zootopia", Year: "2016" },
+    { Movie: "Monty Python and the Holy Grail", Year: "1975" }
   ],
   Romance: [
-    { Song: "The nights", Movie: "" },
-    { Song: "I could be the one", Movie: "" },
-    { Songs: "The day", Movie: "" }
+    { Movie: "It Happened One Night", Year: "1934" },
+    { Movie: "The Shape of Water", Year: "2017" },
+    { Movie: "Slumdog Millionaire", Year: "2008" }
   ],
   Thriller: [
-    { Song: "Love me like you do", Movie: "50 Shades of Grey" },
-    { Song: "I know you care", Movie: "Now Is Good" },
-    { Song: "Beating Heart", Movie: "Halcyon Days" }
+    { Movie: "Don't Breathe", Year: "2016" },
+    { Movie: "The Purge", Year: "2013" },
+    { Movie: "Source Code", Year: "2011" }
   ]
 };
 
 var heading = "Movie Recommendation";
-var subHeading = "Listed here are some of my favourite";
+var subHeading =
+  "Listed here are some of my favourite movies in the different genres";
 
 export default function App() {
   const [genrelist, setGenrelist] = useState("Action");
 
-  function onClickHandler(musician) {
-    setGenrelist(musician);
+  function onClickHandler(genreSelected) {
+    setGenrelist(genreSelected);
   }
 
   return (
@@ -44,28 +45,27 @@ export default function App() {
       <h1>{heading}</h1>
       <h4>{subHeading}</h4>
       <hr />
-      {Object.keys(songsDictionary).map((musician) => {
+      {Object.keys(moviesDictionary).map((genreSelected) => {
         return (
           <button
-            className="musicians"
-            onClick={() => onClickHandler(musician)}
-            key={musician}
+            onClick={() => onClickHandler(genreSelected)}
+            key={genreSelected}
           >
-            {musician}
+            {genreSelected}
           </button>
         );
       })}
       <ul>
-        {songsDictionary[genrelist].map((genre) => {
+        {moviesDictionary[genrelist].map((genre) => {
           return (
-            <li className="musicList" key={genre.Song}>
-              <div className="songDet">
-                <span className="spanText">Song - </span>
-                {genre.Song}
-              </div>
+            <li className="movieList" key={genre.Movie}>
               <div className="songDet">
                 <span className="spanText">Movie - </span>
                 {genre.Movie}
+              </div>
+              <div className="songDet">
+                <span className="spanText">Year - </span>
+                {genre.Year}
               </div>
             </li>
           );
